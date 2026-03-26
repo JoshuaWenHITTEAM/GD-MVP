f4f184b4e28ff6142fb91f17799463018c3fa394
yuanananananan <2754638412@qq.com>
2026-03-26 20:04:30 +0800

    feat: add infer handler for container requests

--- algorithm_platform/kubernetes/internal/api/handler.go
+++ algorithm_platform/kubernetes/internal/api/handler.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"io"
 	"net/http"
 
 	"algo-container-manager/internal/common"
@@ -126,4 +127,46 @@ func (h *Handler) GetContainerStatus(c *gin.Context) {
 	}
 
 	common.Success(c, status)
+}
+
+func (h *Handler) InferContainer(context *gin.Context) {
+	name := context.Param("name")
+	namespace := context.DefaultQuery("namespace", "default")
+
+	fileHeader, err := context.FormFile("file")
+	if err != nil {
+		common.Fail(context, http.StatusBadRequest, "file is required")
+		return
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		common.Fail(context, http.StatusBadRequest, "open uploaded file failed")
+		return
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		common.Fail(context, http.StatusBadRequest, "read upload file failed")
+		return
+	}
+
+	fields := map[string]string{}
+	form, _ := context.MultipartForm()
+	if form != nil {
+		for key, vals := range form.Value {
+			if len(vals) > 0 {
+				fields[key] = vals[0]
+			}
+		}
+	}
+
+	raw, err := h.containerSvc.Infer(name, namespace, fileHeader.Filename, content, fields)
+	if err != nil {
+		common.Fail(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	context.Data(http.StatusOK, "application/json; charset = utf-8", raw)
 }
