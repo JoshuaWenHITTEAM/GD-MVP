f24e72e013ad37e9b7141ba52a20f2a0c08ac6b8
yuanananananan <2754638412@qq.com>
2026-04-07 17:49:24 +0800

    refactor: update deployment labels to use version uuid

--- algorithm_platform/kubernetes/internal/common/labels.go
+++ algorithm_platform/kubernetes/internal/common/labels.go
@@ -1,27 +1,20 @@
 package common
 
-import (
-	"algo-container-manager/internal/model"
-	"fmt"
-)
+import "algo-container-manager/internal/model"
 
-// BuildDeploymentLabels 为 Deployment 创建标签
 func BuildDeploymentLabels(req model.StartAlgorithmRequest) map[string]string {
 	labels := map[string]string{
 		"app": req.DeploymentName,
 	}
 
-	if req.AlgorithmVersionID > 0 {
-		labels["algorithm-version-id"] = fmt.Sprintf("%d", req.AlgorithmVersionID)
-	}
-	if req.ImageID > 0 {
-		labels["image-id"] = fmt.Sprintf("%d", req.ImageID)
-	}
 	if req.Name != "" {
 		labels["algorithm-name"] = req.Name
 	}
 	if req.Version != "" {
 		labels["algorithm-version"] = req.Version
+	}
+	if req.VersionUUID != "" {
+		labels["version-uuid"] = req.VersionUUID
 	}
 
 	return labels
